d0557c17324debca1504ea3490d492597fb9277b
Apedy tech <115509032+Apedy@users.noreply.github.com>
2023-02-21 16:31:05 +0900

    Update and rename behavior_pack/script/@apedy/minecraft.js to behavior_pack/script/lib/minecraft.js

--- behavior_pack/script/lib/minecraft.js
+++ behavior_pack/script/lib/minecraft.js
@@ -1,17 +1,29 @@
 import * as mc from '@minecraft/server';
 
-export const world = {
-	dimension: mc.world.getDimension("overworld"),
+export class World {
+	/**
+	 * @param {string} dimensionId
+	 */
+	constructor(dimensionId) {
+		this.dimension = mc.world.getDimension(dimensionId);
+	}
 	/**
 	 * 複数のコマンドを実行します。
-	 * @param {(mc.Player|mc.Dimension)} caller 実行元
+	 * @param {mc.Player|mc.Dimension} caller 実行元
 	 * @param {...string} syntaxes 構文
 	 */
 	runCommands(caller, ...syntaxes) {
-		syntaxes.forEach(async function(syntax) {
-			await caller.runCommandAsync(syntax);
-		});
-	},
+		caller || this.dimension;
+
+		return syntaxes.map(syntax => caller.runCommandAsync(syntax));
+	}
+	/**
+	 * プレイヤーリストを取得します。
+	 * @returns {mc.Player[]}
+	 */
+	getPlayerList() {
+		return Array.from(mc.world.getPlayers());
+	}
 	/**
 	 * プレイヤーが手に持っているアイテムを返します。
 	 * @param {mc.Player} player プレイヤー
@@ -19,16 +31,15 @@ export const world = {
 	 */
 	hasItem(player) {
 		return player.getComponent("minecraft:inventory").container.getItem(player.selectedSlot);
-	},
+	}
 	/**
 	 * プレイヤーに指定したアイテムを持たせます。
 	 * @param {mc.Player} player プレイヤー
 	 * @param {mc.ItemStack} ItemStack
 	 * @param {?string[]} lore 説明
 	 */
-	giveItem(player, ItemStack, lore) {
-		if (lore) ItemStack.setLore(lore);
-
+	giveItem(player, ItemStack, lore = "") {
+		ItemStack.setLore(lore);
 		player.getComponent("minecraft:inventory").container.addItem(ItemStack);
 	}
-};
+}
