46b154ea20dfd762ba460b185082a85f154f47e0
Apedy tech <115509032+Apedy@users.noreply.github.com>
2023-05-07 02:03:05 +0900

    Update minecraft.js

--- behavior_pack/script/lib/minecraft.js
+++ behavior_pack/script/lib/minecraft.js
@@ -1,3 +1,12 @@
+/*!
+ * minecraft.js v1.0.2
+ *
+ * Copyright (c) 2023 Apedy
+ *
+ * This software is released under the minecraft.js.
+ * see https://opensource.org/license/MIT
+ */
+
 import * as mc from '@minecraft/server';
 
 export class World {
@@ -9,20 +18,20 @@ export class World {
 	}
 	/**
 	 * 複数のコマンドを実行します。
-	 * @param {mc.Player|mc.Dimension} caller 実行元
+	 * @param {(mc.Player|mc.Dimension)?} caller 実行元
 	 * @param {...string} syntaxes 構文
+	 * @returns {Promise<mc.CommandResult>[]}
 	 */
-	runCommands(caller, ...syntaxes) {
-		caller || this.dimension;
-
+	runCommands(caller = this.dimension, ...syntaxes) {
 		return syntaxes.map(syntax => caller.runCommandAsync(syntax));
 	}
 	/**
 	 * プレイヤーリストを取得します。
+	 * @param {mc.EntityQueryOptions?} options
 	 * @returns {mc.Player[]}
 	 */
-	getPlayerList() {
-		return Array.from(mc.world.getPlayers());
+	getPlayerList(options) {
+		return options ? Array.from(mc.world.getPlayers(options)) : mc.world.getAllPlayers();
 	}
 	/**
 	 * プレイヤーが手に持っているアイテムを返します。
@@ -41,5 +50,13 @@ export class World {
 	giveItem(player, ItemStack, lore = "") {
 		ItemStack.setLore(lore);
 		player.getComponent("minecraft:inventory").container.addItem(ItemStack);
+	}
+	/**
+	 * Apply motion to the player.
+	 * @param {mc.Player|mc.Entity} player
+	 * @param {mc.Vector3} vector
+	 */
+	applyMotion(player, { x, y, z }) {
+		player.applyKnockback(x, z, Math.sqrt(x ** 2 + z ** 2) * 0.4, y < 1 ? 0.5 : y * 0.25);
 	}
 }
